cdccbf5b20263d205cbda12f1778beda9e884f8f
Emanuel Henry <emanuelhenrysc@gmail.com>
2025-10-13 10:11:47 -0300

    config: variaveis ambiente

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,15 @@
 import knex from "knex";
 import type { Knex } from "knex";
+import 'dotenv/config' //process.env
+
+if(!process.env.DATABASE_URL){
+  throw new Error('DATABASE_URL env not found.')
+}
 
 export const config: Knex.Config = {
   client: "sqlite3",
   connection: {
-    filename: "./db/app.db",
+    filename: process.env.DATABASE_URL
   },
   useNullAsDefault: true,
   migrations: {
