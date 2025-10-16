8f3dbb67cfdb97e299019f9367a2f5a8c322f9bc
Emanuel Henry <emanuelhenrysc@gmail.com>
2025-10-16 11:42:44 -0300

    feat: implementa middleware para checar existencia de cookie

--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -2,33 +2,61 @@ import {randomUUID} from 'node:crypto';
 import type { FastifyInstance } from "fastify";
 import {z} from 'zod'
 import { db } from "../database.js";
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists.js';
 
 export async function transactionsRoutes(app: FastifyInstance){
     
-    app.get("/", async ()=>{
-        const transactions = await db('transactions').select()
+    app.get("/",
+    {
+        preHandler: [checkSessionIdExists]
+    },
+    async (req, res)=>{
 
-        return {
-            total: 100,
-            transactions,
-        }
+        const sessionId = req.cookies.sessionId
+
+        const transactions = await db('transactions')
+            .where('session_id', sessionId)
+            .select()
+
+        return { transactions }
     });
 
-    app.get("/:id", async (req, res)=>{
+    app.get("/:id", {
+        preHandler: [checkSessionIdExists]
+    }, 
+    async (req, res)=>{
         const getTransactionParamsSchema = z.object({
             id : z.string().uuid(),
         })
 
         const { id } = getTransactionParamsSchema.parse(req.params)
+        
+        const { sessionId } = req.cookies
 
         const transaction = await db('transactions')
-            .where('id', id).first()
+            .where({
+                'session_id': sessionId,
+                id
+            })
+            .first()
+
+        if(!transaction){
+            res.send('Página inacessível...')
+        }
 
         return transaction
     })
 
-    app.get('/sumary', async(req, res)=>{
-        const sumary = await db('transactions').sum('amount', { as: 'amount'}).first()
+    app.get('/sumary', {
+        preHandler: [checkSessionIdExists]
+    }, async(req, res)=>{
+
+        const {sessionId} = req.cookies
+
+        const sumary = await db('transactions')
+            .where('session_id', sessionId)
+            .sum('amount', { as: 'amount'})
+            .first()
 
         return {sumary}
     })
