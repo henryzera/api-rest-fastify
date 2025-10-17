b941157831a22aee84a7989cbfb5cf228fc4b2ec
Emanuel Henry <emanuelhenrysc@gmail.com>
2025-10-17 10:21:58 -0300

    feat: cria primeiro teste

---
+++ test/example.spec.ts
@@ -0,0 +1,8 @@
+import {test, expect} from 'vitest'
+
+test('o usuário consegue criar uma nova transação', () => {
+
+    const responseStatusCode = 201
+
+    expect(responseStatusCode).toEqual(201)
+})
